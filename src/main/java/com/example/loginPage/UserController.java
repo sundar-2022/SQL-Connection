7f4fca951e800404270f9b7c73553b03cafc8495
kanagaraj.pazhani@mothercode.com <kanagaraj.pazhani@mothercode.com>
2023-11-20 17:31:07 +0530

    added findAll function

--- src/main/java/com/example/loginPage/UserController.java
+++ src/main/java/com/example/loginPage/UserController.java
@@ -3,6 +3,8 @@ package com.example.loginPage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -16,5 +18,10 @@ public class UserController {
     @GetMapping("/getUser")
     public User getUser(@RequestParam("id") Integer id){
         return userService.getUser(id);
+    }
+
+    @GetMapping("/getAll")
+    public List<User> getAll(){
+        return userService.getAll();
     }
 }
--- src/main/java/com/example/loginPage/UserService.java
+++ src/main/java/com/example/loginPage/UserService.java
@@ -3,6 +3,8 @@ package com.example.loginPage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
     @Autowired
@@ -15,5 +17,10 @@ public class UserService {
     public User getUser(int id){
        User user = userRepository.findById(id).get();
        return user;
+    }
+
+    public List<User> getAll(){
+        List<User> findAll= userRepository.findAll();
+        return findAll;
     }
 }
